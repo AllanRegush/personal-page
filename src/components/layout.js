a2ad5b58122c4370528202d449a3fb34b80235cd
Allan Regush <17693494+AllanRegush@users.noreply.github.com>
2019-11-26 11:26:54 -0600

    Added Styled Components - Removed Boilerplate code - Started setup with styled components - Tested layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,9 +8,38 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 
 import Header from "./header"
-import "./layout.css"
+
+const theme = {
+  maxWidth: '1000px',
+}
+
+const StyledPage = styled.div`
+  background: lightgray;
+  color: black;
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+`;
+
+injectGlobal`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+`;
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,24 +53,14 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <StyledPage>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <Inner>
+          {children}
+        </Inner>
+      </StyledPage>
+    </ThemeProvider>
   )
 }
 
